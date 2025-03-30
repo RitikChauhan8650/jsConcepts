53c7286e02f4621a4304d791ab2d4aa257051acf
Ritik Chauhan <ritikchauhan8650@gmail.com>
2025-03-30 23:30:20 +0530

    mock20

---
+++ mock20.js
@@ -0,0 +1,61 @@
+///////////1111111111
+// function abc() {
+//   1;
+//   console.log(abc.xyz);
+// }
+
+// abc();
+// abc.xyz = 400;
+// abc.xyz = 200;
+// abc();
+
+// /222222222222222222
+// const numbers = [1, 2, 3, 4];
+// numbers[100] = 500;
+
+// console.log("numbers--------", numbers);
+// console.log("numbers[100]--------", numbers[100]);
+
+//3333333333333333
+// console.log(typeof typeof 100);
+
+//44444444444
+// const arr = [..."ritik"];
+// console.log(arr);
+
+// //5555555555
+// console.log(parseInt("10+2"));
+// console.log(parseInt("7FM"));
+// console.log(parseInt("M7F"));
+
+//6666666666
+// console.log(isNaN("praveen"));
+
+//7777777
+// console.log(
+//   [1, 2].map((num) => {
+//     if (num > 0) return;
+//     return num * 2;
+//   })
+// );
+
+////8888888888888
+// function abc() {
+//   return;
+// }
+// console.log(abc());
+
+/////9999999999999999999
+// {
+//   function abc() {
+//     console.log("Praveen");
+//   }
+// }
+// console.log("function called", abc());
+
+//10000000000000
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+const newArr = arr.map((num) => num > 4);
+const newArr2 = arr.forEach((num) => num > 4);
+console.log(newArr);
+console.log(newArr2);
